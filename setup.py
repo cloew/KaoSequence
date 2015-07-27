a575989e1e96b29c1d03f32eba5264a6b835436b
Chris Loew <cloew123@gmail.com>
2015-07-27 15:58:36 -0600

    Added Boilerplate Package code

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='kao_sequence',
+      version='0.0.1',
+      description="",
+      author='',
+      author_email='',
+      packages=['kao_sequence'],
+     )
